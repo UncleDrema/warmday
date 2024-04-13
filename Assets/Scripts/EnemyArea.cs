8628bdcdac479655838c7b5fdfcd1d9d97049165
Kirill <missl.wipiece@gmail.com>
2024-04-13 12:29:14 +0300

    added zombis

---
+++ Assets/Scripts/EnemyArea.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using TriInspector;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    [RequireComponent(typeof(PolygonCollider2D))]
+    public class EnemyArea : MonoBehaviour
+    {
+        public List<Enemy> Prefabs;
+        [SerializeField]
+        private PolygonCollider2D poly;
+        
+        private void Awake()
+        {
+            poly = GetComponent<PolygonCollider2D>();
+        }
+
+        public Vector2 GetRandomPosition()
+        {
+            var bounds = poly.bounds;
+            Vector2 from = bounds.min;
+            Vector2 to = bounds.max;
+
+            Vector2 p = GenRandom(from, to);
+            while (!poly.OverlapPoint(p))
+            {
+                p = GenRandom(from, to);
+            }
+
+            return p;
+        }
+
+        [Button]
+        public void SpawnEnemy()
+        {
+            var prefab = Prefabs[Random.Range(0, Prefabs.Count)];
+            var go = Instantiate(prefab, GetRandomPosition(), Quaternion.identity, transform);
+            go.area = this;
+        }
+
+        private Vector2 GenRandom(Vector2 min, Vector2 max)
+        {
+            return new Vector2(Random.Range(min.x, max.x), Random.Range(min.y, max.y));
+        }
+    }
+}
--- Assets/Scripts/NightCamera.cs
+++ Assets/Scripts/NightCamera.cs
@@ -8,24 +8,51 @@ namespace Game
     public class NightCamera : MonoBehaviour
     {
         private static readonly int FlashlightKey = Shader.PropertyToID("_Flashlight");
+        private static readonly int PlayerLightKey = Shader.PropertyToID("_LightSourcePlayer");
         
         public Transform player;
+        public Transform flashlight;
         public Material nightMaterial;
+        private Camera _camera;
         private Vector3 offset;
+        private float4 savedFlashlight;
+        private float4 savedPlayer;
 
         private void Awake()
         {
+            _camera = GetComponent<Camera>();
             offset = transform.position - player.position;
-            nightMaterial = new Material(nightMaterial);
+            savedFlashlight = nightMaterial.GetVector(FlashlightKey);
+            savedPlayer = nightMaterial.GetVector(PlayerLightKey);
+        }
+
+        private void OnApplicationQuit()
+        {
+            nightMaterial.SetVector(FlashlightKey, savedFlashlight);
+            nightMaterial.SetVector(PlayerLightKey, savedPlayer);
         }
 
         private void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
-            var rot = player.rotation.eulerAngles.z;
-            float4 flashlight = nightMaterial.GetVector(FlashlightKey);
-            flashlight.x = rot;
-            nightMaterial.SetVector(FlashlightKey, flashlight);
+            UpdatePosition();
+            UpdateRotation();
             Graphics.Blit(source, destination, nightMaterial);
+        }
+
+        private void UpdatePosition()
+        {
+            Vector2 screen = _camera.WorldToViewportPoint(flashlight.position);
+            float4 playerLight = nightMaterial.GetVector(PlayerLightKey);
+            playerLight.xy = screen;
+            nightMaterial.SetVector(PlayerLightKey, playerLight);
+        }
+
+        private void UpdateRotation()
+        {
+            var rot = player.rotation.eulerAngles.z - 90;
+            float4 flashlightPar = nightMaterial.GetVector(FlashlightKey);
+            flashlightPar.x = rot;
+            nightMaterial.SetVector(FlashlightKey, flashlightPar);
         }
 
         private void LateUpdate()
